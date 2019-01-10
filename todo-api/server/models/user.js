4533d6a7c0b2abbdfcc4df4812e3fc3f3b83bbac
Shawn Doyle <shawndoyl@gmail.com>
2019-01-10 13:23:01 -0500

    Add DELETE /users/me/token route

--- todo-api/server/models/user.js
+++ todo-api/server/models/user.js
@@ -49,6 +49,15 @@ UserSchema.methods.generateAuthToken = function() {
 	});
 };
 
+UserSchema.methods.removeToken = function (token) {
+	const user = this;
+	return user.update({
+		$pull: {
+			tokens: {token}
+		}
+	});
+};
+
 UserSchema.statics.findByToken = function (token) {
 	let User = this;
 	let decoded;
